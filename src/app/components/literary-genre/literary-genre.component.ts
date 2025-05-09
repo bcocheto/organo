c0d08c683bdf5b32a572d030383d47fe005e473e
Breno Cocheto <breno.cocheto@gmail.com>
2025-05-09 13:51:04 -0300

    feat(genre): style genre sections
    - Added genre-specific styling to book & genre components
    - Improved visual distinction between genres
    - Used CSS classes for dynamic styling
    - Added background colors and borders
    - Updated component HTML and CSS

--- src/app/components/literary-genre/literary-genre.component.ts
+++ src/app/components/literary-genre/literary-genre.component.ts
@@ -1,12 +1,12 @@
 import { Component, input } from '@angular/core';
 import { BookComponent } from '../book/book.component';
-import { books } from '../../../books-mock';
 import { Book } from '../book/book';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-literary-genre',
   standalone: true,
-  imports: [BookComponent],
+  imports: [BookComponent, CommonModule],
   templateUrl: './literary-genre.component.html',
   styleUrl: './literary-genre.component.css',
 })
